f64720f2628d96a4706955500524c4f1126d197b
Bobablo12 <127266078+Bobablo12@users.noreply.github.com>
2025-11-23 11:40:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,9 @@ with open(LABELS_PATH, "r") as f:
     labels = json.load(f)
 
 # Root endpoint
-
+@app.get("/")
+def root():
+    return {"message": "Lung Cancer Prediction API is running!"}
 
 # Health endpoint
 @app.get("/health")
